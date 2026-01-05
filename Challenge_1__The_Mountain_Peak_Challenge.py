bea314c5fe64de4a355aaa1187c5772a68f39b29
Syed Abdul Ali Zaidi <syedabdulali25@gmail.com>
2026-01-05 23:20:10 +0500

    Add files via upload

---
+++ Challenge_1__The_Mountain_Peak_Challenge.py
@@ -0,0 +1,11 @@
+def count_ways_to_summit(n : int):
+    if n <= 1:
+        return 1
+
+    a = 1
+    b = 1
+    for i in range(2,n+1):
+        c = a + b
+        a = b
+        b = c
+    return c
