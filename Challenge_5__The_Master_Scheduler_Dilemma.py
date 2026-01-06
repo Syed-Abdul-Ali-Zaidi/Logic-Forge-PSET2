f47048dc9169af12c99b1d99c24ca6fa000f69de
Syed Abdul Ali Zaidi <syedabdulali25@gmail.com>
2026-01-06 21:24:57 +0500

    Add files via upload

--- Challenge_5__The_Master_Scheduler_Dilemma.py
+++ Challenge_5__The_Master_Scheduler_Dilemma.py
@@ -1,13 +1,15 @@
 def min_cancelled_bookings(intervals : list):
-    # Sorting the Start Time
+    # Sorting w.r.t Start Time
     intervals.sort(key= lambda x : x[0])
 
     previous_end_time = intervals[0][1]
+    intervals_to_remove = 0
     for i in range(1,len(intervals)):
         if intervals[i][0] < previous_end_time:
-            intervals.pop(i)
-    return intervals
+            intervals_to_remove += 1
+
+    return intervals_to_remove
 
 
-
-min_cancelled_bookings([[1, 2], [2, 3], [3, 4], [1, 3]])
+a =min_cancelled_bookings([[1, 2], [5, 10], [18, 35]])
+print(a)
