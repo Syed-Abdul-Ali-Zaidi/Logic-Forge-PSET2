dcc03c176ed43af7e5e404112e5d16f886932d22
Syed Abdul Ali Zaidi <syedabdulali25@gmail.com>
2026-01-08 22:20:47 +0500

    Update Challenge_6__The_High-Stakes_Freelancer.py

--- Challenge_6__The_High-Stakes_Freelancer.py
+++ Challenge_6__The_High-Stakes_Freelancer.py
@@ -1,21 +1,26 @@
 def maximize_freelance_profit(deadlines, profits):
     n  = len(deadlines)
+
+    # Making pairs of [deadline, job]
     jobs = []
     for i in range(n):
         jobs += [[ deadlines[i], profits[i] ]]
-    jobs.sort()
+
+    # Sorting pairs in Desc wrt Profit
+    jobs.sort(key=lambda x: x[1], reverse=True)
 
     Profits = 0
     Job_count = 0
-    Last_deadline = 0
-    Max_deadline = jobs[-1][0]
-    print(Max_deadline)
-    for i in range(n-1,-1,-1):
-        current_job = jobs[i]
-        if Max_deadline != 0 and Last_deadline != 1:
-            Profits += current_job[1]
-            Job_count += 1
-            Last_deadline = current_job[0]
-            Max_deadline -= 1
+    Max_deadline = max(i[0] for i in jobs)
+    slots = [False]  * Max_deadline
+
+    for job in jobs:
+        deadline = job[0] - 1   # "-1" for indexing purpose
+        for j in range(deadline,-1,-1):
+            if slots[j] == False:
+                slots[j] = True
+                Profits += job[1]
+                Job_count += 1
+                break
 
     return f'[{Job_count} Jobs, {Profits} Profit]'
