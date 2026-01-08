40661aa681aa45afdf8f5de9567f383a9930609b
Syed Abdul Ali Zaidi <syedabdulali25@gmail.com>
2026-01-08 21:25:58 +0500

    Update Challenge_6__The_High-Stakes_Freelancer.py

--- Challenge_6__The_High-Stakes_Freelancer.py
+++ Challenge_6__The_High-Stakes_Freelancer.py
@@ -1,6 +1,18 @@
 def maximize_freelance_profit(deadlines, profits):
+    n  = len(deadlines)
     jobs = []
-    for i in range(len(deadlines)):
+    for i in range(n):
         jobs += [[ deadlines[i], profits[i] ]]
     jobs.sort()
-    print(jobs)
+
+    Profits = 0
+    Job_count = 0
+    Last_deadline = 0
+    for i in range(n-1,-1,-1):
+        current_job = jobs[i]
+        if current_job[0] != Last_deadline:
+            Profits += current_job[1]
+            Job_count += 1
+            Last_deadline = current_job[0]
+
+    return f'[{Job_count} Jobs, {Profits} Profit]'
