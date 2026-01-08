4c1086c8c62c5e78298f3ef104f8161004bc93e0
Syed Abdul Ali Zaidi <syedabdulali25@gmail.com>
2026-01-08 22:30:27 +0500

    Add files via upload

---
+++ Challenge_7__Koko_Banana_Buffet.py
@@ -0,0 +1,17 @@
+def calculate_minimum_speed(piles : list,k : int):
+    Min = 1
+    Max = max(piles)
+    while Min != Max:
+        speed = (Min + Max) // 2
+        total_hours = 0
+        for pile in piles:
+            hrs_per_pile = (pile + speed - 1) // speed # Finding ceiling of pile/speed
+            total_hours += hrs_per_pile
+        if total_hours <= k:
+            Max = speed
+        else:
+            Min = speed + 1
+    return Max
+
+# Piles = [5, 10, 3]
+# print(calculate_minimum_speed(Piles,4))
