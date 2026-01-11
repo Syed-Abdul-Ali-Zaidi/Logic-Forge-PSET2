367eafe37efd81d6e479d806d4868d5ef3845ff4
Syed Abdul Ali Zaidi <syedabdulali25@gmail.com>
2026-01-11 20:48:24 +0500

    Update Challenge_4__The_Royal_Treasury.py

--- Challenge_4__The_Royal_Treasury.py
+++ Challenge_4__The_Royal_Treasury.py
@@ -1,19 +1,19 @@
 def count_payment_combinations(coins, total_sum):
     arr = []
     coin_count = len(coins)
-    for i in range(coin_count): # Creating 2D_arr where cols = 0 to total_sum & rows = no._of_coins
+    for i in range(coin_count + 1): # Creating 2D_arr where cols = (0 to total_sum) & rows = (1 row with all 0s) + No._of_coins
         arr += [[0 for j in range(total_sum+1)]]
 
-    for i in range(coin_count):
+    for i in range(1, coin_count + 1):
         arr[i][0] = 1   # for total_sum=0 there will be always 1 way no matter what the coin is.
         for j in range(1,total_sum+1):
-            if coins[i] > j:
+            if coins[i-1] > j:  # Exclude current coin
                 arr[i][j] = arr[i-1][j]
-            else:
-                arr[i][j] = arr[i-1][j] + arr[i][j-coins[i]]
+            else:   # Include current coin
+                arr[i][j] = arr[i-1][j] + arr[i][j-coins[i-1]]
 
     # Bottom_right element of 2D_arr will be the answer
-    return arr[coin_count-1][total_sum]
+    return arr[coin_count][total_sum]
 
 # Done!
-print(count_payment_combinations([2],3))
+# print(count_payment_combinations([2],3))
